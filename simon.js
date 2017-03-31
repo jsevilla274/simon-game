9944b19ef4f64baa3a724408244c1fc9c62afbfd
John Sevilla <jsevilla274@gmail.com>
2017-03-31 00:06:53 -0400

    Add Game definition and initialize properties

--- simon.js
+++ simon.js
@@ -1,1 +1,24 @@
-$(function() {})
+function Game() {
+	var sequence = [];
+	var green = $("#green");
+	var red = $("#red");
+	var blue = $("#blue");
+	var yellow = $("#yellow");
+
+	var colorElement = function(elementID, active) {
+		//switch
+	}
+
+	var randomizer = function() {
+		//number 0-3
+		return Math.floor(Math.random() * 4);
+	}
+
+	this.animateSequence = function() {
+		var elemID = randomizer();
+		sequence.push(elemID);
+	}
+
+}
+
+$(function() {});
